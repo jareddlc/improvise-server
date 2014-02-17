a79172e14476a9760f070f7cb5e6716de07f47a9
Jared <jared_de_la_cruz@yahoo.com>
2014-02-16 17:15:46 -0800

    basic upload

--- app.js
+++ app.js
@@ -2,20 +2,25 @@ var express = require('express');
 var routes = require('./routes');
 var http = require('http');
 var path = require('path');
+var swig = require('swig');
 
 var app = express();
+
+// swig
+app.engine('html', swig.renderFile);
 
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
-//app.set('view engine', 'html');
+app.set('view engine', 'html');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
-app.use(express.cookieParser('your secret here'));
+app.use(express.cookieParser('improvise'));
 app.use(express.session());
+app.use(express.bodyParser()); // figure out solution to remove this. used for multipart upload
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,16 @@
 var upload = require('../lib/upload');
 
 exports.index = function(req, res) {
-  res.send({hello: 'world!'});
+  res.render('index', { title: 'Improvise Server'});
 };
 
 exports.upload = function(req, res) {
-  upload.uploadFile(req, res, function() {
-    res.send({upload: 'upload'});
+  upload.uploadFile(req, res, function(err) {
+    if(err) {
+      res.send({upload: 'failed'});
+    }
+    else {
+      res.send({upload: 'sucess'});
+    }
   });
 };
